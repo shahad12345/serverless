3d497665d46c360b5a59be67efda5f1ae781b9f4
Hussam Al-Zughaibi <hs.alzughaibi@takamol.com.sa>
2018-03-22 14:50:16 +0300

    Create a table to contain the trainees.

--- handler.js
+++ handler.js
@@ -1,7 +1,13 @@
 'use strict';
 
+// Libraries.
+const uuid = require('uuid');
 const AWS = require('aws-sdk');
 const SES = new AWS.SES();
+const DynamoDB = new AWS.DynamoDB.DocumentClient();
+
+// Constants.
+const program = 'summer-2018';
 
 function makeResponse(statusCode) {
   return {
@@ -14,8 +20,23 @@ function makeResponse(statusCode) {
 
 // https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
 function validateEmail(email) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+function validateMobile(mobile) {
+  var re = /^\+\d+$/;
+  return re.test(mobile);
+}
+
+function validateDate(date) {
+  var re = /^\d{4}\-\d{2}-\d{2}$/;
+  return re.test(date);
+}
+
+function validateYoutubeVideoUrl(youtubeVideoUrl) {
+  var re = /^http(s)?:\/\/(www\.)?youtu(be\.com|.be)\/.+$/;
+  return re.test(youtubeVideoUrl);
 }
 
 module.exports.contactUs = (event, context, callback) => {
@@ -59,9 +80,123 @@ module.exports.contactUs = (event, context, callback) => {
     ReplyToAddresses: [email]
   };
 
-  SES.sendEmail(emailParams, function (error, response) {
+  SES.sendEmail(emailParams, (error, response) => {
     console.log('error', error);
     console.log('response', response);
     return callback(null, makeResponse(error ? 408 : 204));
   });
 };
+
+// trainees
+//   id
+//   fullname
+//   gender
+//   email
+//   mobile
+//   university
+//   major
+//   location
+//   expectedGraduationDate
+//   youtubeVideoUrl
+//   howDidYouKnowAboutUs
+//   statuses
+//     applied, when
+//     voteAdded <- 
+//     votesCalculated
+//     initiallyAccepted, when, whome
+//     initiallyRejected, when, whome
+//     waitingForDocuments
+//     providedDocuments, documents
+//     accepted
+//     rejected
+//     droppedOut
+//     kickedOut
+//     completed
+//     passed
+//     candidated
+
+module.exports.apply = (event, context, callback) => {
+
+  try {
+    var body = JSON.parse(event.body);
+  } catch (error) {
+    var body = null;
+  }
+
+  const id = uuid.v4();
+  const timestamp = new Date().getTime();
+  const fullname = body ? body.fullname : null;
+  var gender = body ? body.gender : null;
+  const email = body ? body.email : null;
+  const mobile = body ? body.mobile : null;
+  const university = body ? body.university : null;
+  const major = body ? body.major : null;
+  const location = body ? body.location : null;
+  const expectedGraduationDate = body ? body.expectedGraduationDate : null;
+  const youtubeVideoUrl = body ? body.youtubeVideoUrl : null;
+  const howDidYouKnowAboutUs = body ? body.howDidYouKnowAboutUs : null;
+
+  if (!fullname || !gender || !email || !mobile || !university || !major || !location || !expectedGraduationDate || !youtubeVideoUrl || !howDidYouKnowAboutUs) {
+    return callback(null, makeResponse(400));
+  }
+
+  // Validate email, mobile, expectedGraduationDate, and youtubeVideoUrl.
+  if (validateEmail(email) === false || validateMobile(mobile) == false || validateDate(expectedGraduationDate) == false || validateYoutubeVideoUrl(youtubeVideoUrl) == false) {
+    return callback(null, makeResponse(406));
+  }
+  
+  const checkIfTraineeAlreadyExists = (callback) => {
+    const scanParams = {
+      TableName: 'trainees',
+      FilterExpression: 'attribute_not_exists(deletedAt) and program = :program and (email = :email or mobile = :mobile)',
+      ExpressionAttributeValues: {
+        ':program': program,
+        ':email' : email,
+        ':mobile' : mobile,
+      },
+    };
+    DynamoDB.scan(scanParams, (error, result) => {
+      return (error || result.Count > 0) ? callback(true) : callback(false);
+    });
+  };
+
+  const addTrainee = (callback) => {
+    gender = (gender == 'male') ? 'male' : 'female';
+    const putParams = {
+      TableName: 'trainees',
+      Item: {
+        id: id,
+        program: program,
+        fullname: fullname,
+        gender: gender,
+        email: email,
+        mobile: mobile,
+        university: university,
+        major: major,
+        location: location,
+        expectedGraduationDate: expectedGraduationDate,
+        youtubeVideoUrl: youtubeVideoUrl,
+        howDidYouKnowAboutUs: howDidYouKnowAboutUs,
+        statuses: [
+          {
+            type: 'applied',
+            when: timestamp,
+          }
+        ],
+        createdAt: timestamp,
+        updatedAt: timestamp,
+      },
+    };
+    DynamoDB.put(putParams, (error) => {
+      return callback(error);
+    });
+  }
+
+  // Check if the trainee is already there.
+  checkIfTraineeAlreadyExists((exists) => {
+    if (exists) return callback(null, makeResponse(409));
+    addTrainee((error) => {
+      return callback(null, makeResponse(error ? 408 : 204));
+    });
+  });
+}
